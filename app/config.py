abf04daa4b065d7f1e4307d58dccfbda5738d6fe
Alejandro_p <39816912+minosr3@users.noreply.github.com>
2023-11-14 07:08:54 -0500

    Delete app directory

--- app/config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-
-class Config(object):
-    SECRET_KEY = 'tu_clave_secreta_aqui'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///tickets.db'  # O tu URL de base de datos
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
